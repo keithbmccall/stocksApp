4c39b997b6e821a0caeb30fe23c9cd7223a86df6
Keith McCall <keithmccall@Keiths-MacBook-Pro.local>
2018-06-07 22:55:08 -0400

    killer if statement

--- src/components/previews/charts/PreviewChart.js
+++ src/components/previews/charts/PreviewChart.js
@@ -13,22 +13,15 @@ import {
 //
 const PreviewChart = props => {
   const { chartData } = props;
-  console.log(chartData);
+  const data = chartData.chart;
+  console.log(data);
   return (
     <Fragment>
       <div className="container-fluid mb-4">
         <div className="h4">{chartData.companyName}</div>
         <ResponsiveContainer width="100%" height={300}>
           <AreaChart
-            data={[
-              { name: "Page A", uv: 4000, pv: 8000, amt: 2400 },
-              { name: "Page B", uv: 3000, pv: 1398, amt: 2210 },
-              { name: "Page C", uv: 2000, pv: 9800, amt: 2290 },
-              { name: "Page D", uv: 2780, pv: 3908, amt: 2000 },
-              { name: "Page E", uv: 1890, pv: 4800, amt: 2181 },
-              { name: "Page F", uv: 2390, pv: 3800, amt: 2500 },
-              { name: "Page G", uv: 3490, pv: 4300, amt: 2100 }
-            ]}
+            data={data}
             margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
           >
             <defs>
@@ -41,20 +34,19 @@ const PreviewChart = props => {
                 <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
               </linearGradient>
             </defs>
-            <XAxis dataKey="name" />
-            <YAxis />
+            <XAxis dataKey="minute" />
+            <YAxis
+              domain={[
+                dataMin => Math.floor(dataMin),
+                dataMax => Math.floor(dataMax + 1)
+              ]}
+            />
             <CartesianGrid strokeDasharray="3 3" />
             <Tooltip />
+
             <Area
               type="monotone"
-              dataKey="uv"
-              stroke="#8884d8"
-              fillOpacity={1}
-              fill="url(#colorUv)"
-            />
-            <Area
-              type="monotone"
-              dataKey="pv"
+              dataKey="marketAverage"
               stroke="#82ca9d"
               fillOpacity={1}
               fill="url(#colorPv)"
--- src/store/reducers/stocksReducer.js
+++ src/store/reducers/stocksReducer.js
@@ -8,11 +8,25 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case QUERY_STOCKS:
-      return {
-        ...state,
-        info: action.payload.all,
-        chartData: action.payload.chartData.data
-      };
+      return state.chartData.chart
+        ? { ...state, info: action.payload.all }
+        : {
+            ...state,
+            info: action.payload.all,
+            chartData: action.payload.chartData.data
+          };
+    // if (state.chartData.chart) {
+    //   return {
+    //     ...state,
+    //     info: action.payload.all
+    //   };
+    // } else {
+    //   return {
+    //     ...state,
+    //     info: action.payload.all,
+    //     chartData: action.payload.chartData.data
+    //   };
+    // }
     case UPDATE_PREVIEW_CHART:
       return {
         ...state,
